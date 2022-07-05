a6b461f1169cda7bc6349bb833d84d200e839cf2
NaaelL <90194739+NaaelL@users.noreply.github.com>
2022-07-05 18:58:21 +0400

    Add files via upload

---
+++ Digital ATM.py
@@ -0,0 +1,16 @@
+#I'm stuck at the newUser part
+class ATM:
+    def __init__(self, cardNumber, pin):
+        self.cardNumber = cardNumber
+        self.pin = pin
+    def balance ():
+        currentBalance = 10000
+        print ("Your current balance is", currentBalance, "AED")
+    def withdraw ():
+        currentBalance = 10000
+        withdrawlAmount = int (input ("How much money would you like to withdraw? "))
+        currentBalance -= withdrawlAmount
+        print ("Your current balance is", currentBalance, "AED")
+    balance ()
+    withdraw ()
+newUser = ATM (20, 10)
